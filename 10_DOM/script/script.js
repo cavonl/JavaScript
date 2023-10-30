9a02836ac5d20875a0bf65504d86a617fa764f99
cavon <425247833@qq.com>
2023-10-30 21:17:31 +0800

    文档的加载

---
+++ 10_DOM/script/script.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: cavon 425247833@qq.com
+ * @Date: 2023-10-30 21:05:41
+ * @LastEditors: cavon 425247833@qq.com
+ * @LastEditTime: 2023-10-30 21:05:56
+ * @FilePath: /JavaScript/10_DOM/script/script.js
+ */
+let btn = document.getElementById("btn");
+console.log(btn);
